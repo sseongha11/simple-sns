4e421c5e257af28505323230587ccabc6541dd43
Seongha <sseongha11@gmail.com>
2024-04-14 12:59:26 +0100

    init: initialise the project

---
+++ src/test/java/com/recommendation/sns/SimpleSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommendation.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
